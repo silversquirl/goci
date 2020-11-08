6d33aa086dd50dc0a8d525095ee5435edaa46cb0
Samadi van Koten <samadi@vktec.org.uk>
2020-11-08 07:22:35 +0000

    Write simple CI server for building Go programs

---
+++ project.go
@@ -0,0 +1,210 @@
+//go:generate stringer -linecomment -type BuildStatus
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"sync/atomic"
+)
+
+type Project struct {
+	Name  string
+	URL   string
+	path  string
+	build map[string]*Build
+}
+
+func OpenProject(name, path string) (proj *Project, err error) {
+	path, err = filepath.Abs(path)
+	if err != nil {
+		return nil, err
+	}
+
+	proj = &Project{
+		Name: name, path: path,
+		build: make(map[string]*Build),
+	}
+
+	if _, err = os.Stat(proj.path); err != nil {
+		return nil, err
+	}
+
+	proj.URL, err = proj.exec("git", "remote", "get-url", "origin")
+	if err != nil {
+		return nil, err
+	}
+
+	return proj, nil
+}
+
+type GitError []byte
+
+func (e GitError) Error() string {
+	return "git error: " + string(e)
+}
+
+func (proj *Project) exec(name string, arg ...string) (string, error) {
+	cmd := exec.Command(name, arg...)
+	cmd.Dir = proj.path
+	out, err := cmd.Output()
+	switch err := err.(type) {
+	case nil:
+	case *exec.ExitError:
+		return "", GitError(err.Stderr)
+	default:
+		return "", err
+	}
+	return strings.TrimRight(string(out), "\n"), nil
+}
+
+func (proj *Project) Ref(ref string) (string, error) {
+	return proj.exec("git", "rev-parse", "--short", ref)
+}
+
+type Build struct {
+	Proj *Project
+	Ref  string
+	Desc string
+	path string
+
+	CodePath, FilesPath string
+
+	status   int32
+	buildCmd *exec.Cmd
+	buildLog string
+}
+
+func (proj *Project) GetBuild(ref string) (*Build, error) {
+	build, ok := proj.build[ref]
+	if !ok {
+		path := filepath.Join(proj.path, "goci", ref)
+		if err := os.MkdirAll(path, 0777); err != nil {
+			return nil, err
+		}
+
+		desc, err := proj.exec("git", "show-branch", "--no-name", "--", ref)
+		if err != nil {
+			return nil, err
+		}
+
+		build = &Build{
+			Proj: proj, Ref: ref, Desc: desc, path: path,
+			CodePath:  filepath.Join(path, "code"),
+			FilesPath: filepath.Join(path, "files"),
+		}
+		proj.build[ref] = build
+	}
+	return build, nil
+}
+
+func (build *Build) StartBuild() {
+	if !atomic.CompareAndSwapInt32(&build.status, int32(BuildNotStarted), int32(BuildInProgress)) {
+		return
+	}
+
+	go func() {
+		var err error
+		status := BuildFailed
+
+		defer func() {
+			if err != nil {
+				build.buildLog = err.Error()
+				status = BuildFailed
+			}
+			if status == BuildFailed {
+				os.RemoveAll(build.FilesPath)
+			}
+			atomic.StoreInt32(&build.status, int32(status))
+		}()
+
+		codeErr := os.Mkdir(build.CodePath, 0777)
+		filesErr := os.Mkdir(build.FilesPath, 0777)
+		if os.IsExist(codeErr) {
+			if os.IsExist(filesErr) {
+				status = BuildFinished
+			} else {
+				status = BuildFailed
+			}
+			return
+		} else if codeErr != nil {
+			err = codeErr
+			return
+		} else if filesErr != nil {
+			err = filesErr
+			return
+		}
+
+		_, err = build.Proj.exec("git", "--work-tree", build.CodePath, "checkout", "--detach", build.Ref)
+		if err != nil {
+			return
+		}
+		_, err = build.Proj.exec("git", "--work-tree", build.CodePath, "reset", "--hard")
+		if err != nil {
+			return
+		}
+
+		build.buildCmd = exec.Command("go", "build", "-o", filepath.Join(build.FilesPath, build.Proj.Name))
+		build.buildCmd.Dir = build.CodePath
+
+		out, err := build.buildCmd.CombinedOutput()
+		build.buildLog = string(out)
+		switch e := err.(type) {
+		case nil:
+			status = BuildFinished
+		case *exec.ExitError:
+			build.buildLog += "\n" + e.Error()
+			status = BuildFailed
+			err = nil
+		default:
+			status = BuildFailed
+		}
+	}()
+}
+
+func (build *Build) Status() BuildStatus {
+	return BuildStatus(atomic.LoadInt32(&build.status))
+}
+
+func (build *Build) Log() string {
+	switch build.Status() {
+	case BuildFinished, BuildFailed:
+		return build.buildLog
+	default:
+		return ""
+	}
+}
+
+func (build *Build) Summary() BuildSummary {
+	return BuildSummary{
+		build.Proj.Name,
+		build.Proj.URL,
+		build.Ref,
+		build.Desc,
+		build.Status(),
+		build.Log(),
+	}
+}
+
+type BuildStatus int32
+
+const (
+	BuildNotStarted BuildStatus = iota // Not started
+	BuildInProgress                    // In progress
+	BuildFinished                      // Finished
+	BuildFailed                        // Failed
+)
+
+func (status BuildStatus) MarshalText() ([]byte, error) {
+	return []byte(status.String()), nil
+}
+
+type BuildSummary struct {
+	ProjName string      `json:"projectName"`
+	ProjURL  string      `json:"projectURL"`
+	CommitID string      `json:"commit"`
+	Summary  string      `json:"commitSummary"`
+	Status   BuildStatus `json:"status"`
+	Log      string      `json:"buildLog,omitempty"`
+}
--- serve.go
+++ serve.go
@@ -1,50 +1,139 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"strings"
 )
 
-type ci struct{}
-
-func newCI() (ci ci) {
-	return
+type CI struct {
+	Path string
+	proj map[string]*Project
 }
 
-func splitFirst(path string) (first, rest string) {
-	path = strings.TrimPrefix(path, "/")
-	i := strings.Index(path, "/")
-	return path[:i], path[i+1:]
+func NewCI(root string) *CI {
+	return &CI{root, make(map[string]*Project)}
 }
 
-var actions = map[string]func(ci ci, project, ref string, w http.ResponseWriter, r *http.Request){
-	"":      ci.status,
-	"build": ci.build,
-	"files": ci.files,
+func splitFirst(route string) (first, rest string) {
+	route = strings.TrimPrefix(route, "/")
+	i := strings.Index(route, "/")
+	if i < 0 {
+		return route, ""
+	}
+	return route[:i], route[i+1:]
 }
 
-func (ci ci) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	project, path := splitFirst(r.URL.Path)
-	ref, path := splitFirst(path)
-	action, path := splitFirst(path)
-	if hf, ok := actions[action]; ok {
-		hf(ci, project, ref, w, r)
-	} else {
+var actions = map[string]func(ci *CI, build *Build, route string, w http.ResponseWriter, r *http.Request){
+	"":      (*CI).Status,
+	"files": (*CI).Files,
+}
+
+func (ci *CI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	project, rest := splitFirst(r.URL.Path)
+	ref, rest := splitFirst(rest)
+	action, rest := splitFirst(rest)
+
+	actionFunc, ok := actions[action]
+	if !ok {
 		http.NotFound(w, r)
+		return
+	}
+
+	if project == "" || project[0] == '.' {
+		http.NotFound(w, r)
+		return
+	}
+
+	if ref == "" {
+		ref = "master"
+	} else if ref[0] == '-' {
+		http.NotFound(w, r)
+		return
+	}
+
+	proj, err := ci.Project(project)
+	if err != nil {
+		log.Print(err)
+		http.NotFound(w, r)
+		return
+	}
+
+	actualRef, err := proj.Ref(ref)
+	if err != nil {
+		log.Print(err)
+		http.NotFound(w, r)
+		return
+	}
+	if actualRef != ref {
+		u := r.URL
+		u.Path = path.Join("/", project, actualRef, action, rest)
+		http.Redirect(w, r, u.String(), http.StatusTemporaryRedirect)
+		return
+	}
+
+	build, err := proj.GetBuild(ref)
+	if err != nil {
+		log.Print(err)
+		http.NotFound(w, r)
+		return
+	}
+
+	build.StartBuild()
+	actionFunc(ci, build, rest, w, r)
+}
+
+func (ci *CI) Status(build *Build, route string, w http.ResponseWriter, r *http.Request) {
+	if route != "" {
+		http.NotFound(w, r)
+		return
+	}
+	json.NewEncoder(w).Encode(build.Summary())
+}
+
+func (ci *CI) Files(build *Build, route string, w http.ResponseWriter, r *http.Request) {
+	if build.Status() != BuildFinished {
+		http.NotFound(w, r)
+		return
+	}
+
+	if route == "" {
+		dir, err := os.Open(build.FilesPath)
+		if err != nil {
+			log.Print(err)
+			http.NotFound(w, r)
+			return
+		}
+		names, err := dir.Readdirnames(-1)
+		if err != nil {
+			log.Print(err)
+			http.NotFound(w, r)
+			return
+		}
+		json.NewEncoder(w).Encode(names)
+	} else {
+		route = path.Clean(route)
+		http.ServeFile(w, r, filepath.Join(build.FilesPath, route))
 	}
 }
 
-func (ci ci) status(project, ref string, w http.ResponseWriter, r *http.Request) {
-}
-
-func (ci ci) build(project, ref string, w http.ResponseWriter, r *http.Request) {
-}
-
-func (ci ci) files(project, ref string, w http.ResponseWriter, r *http.Request) {
+func (ci *CI) Project(name string) (proj *Project, err error) {
+	proj, ok := ci.proj[name]
+	if !ok {
+		proj, err = OpenProject(name, filepath.Join(ci.Path, name+".git"))
+		if err != nil {
+			return nil, err
+		}
+		ci.proj[name] = proj
+	}
+	return proj, nil
 }
 
 func main() {
-	ci := newCI()
+	ci := NewCI("goci")
 	log.Fatal(http.ListenAndServe(":8080", ci))
 }
